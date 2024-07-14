bf87034e95d0f506c728de82506be0e1943f9ad6
kamser0415 <kamser0415@gmail.com>
2024-07-14 22:22:46 +0900

    fix: GitHub Actions wrapper 추가

--- src/main/java/shop/sendbox/sendbox/hello/controller/HelloController.java
+++ src/main/java/shop/sendbox/sendbox/hello/controller/HelloController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, Sendbox!";
-    }
+	@GetMapping("/hello")
+	public String hello() {
+
+		return "Hello, Sendbox!";
+
+	}
 }
